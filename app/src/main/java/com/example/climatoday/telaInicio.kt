4952a38fd42e6e0df08d1bca630ade03aedb518a
Samuel Gomes <107943580+MucaGomes@users.noreply.github.com>
2022-06-25 13:28:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/climatoday/telaInicio.kt
@@ -0,0 +1,35 @@
+package com.example.climatoday
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+
+
+    class telaInicio : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_tela_inicio)
+
+
+            var btSearch = findViewById<Button>(R.id.button_search) as Button
+
+            btSearch.setOnClickListener {
+                startActivity(Intent(this, MainActivity::class.java))
+
+                var edtText = findViewById(R.id.cidade_digitada) as EditText
+                var texto = edtText.text.toString()
+
+                var Resultado = texto
+
+                    com.example.climatoday.CITY = Resultado
+
+            }
+
+
+        }
+
+    }
+
